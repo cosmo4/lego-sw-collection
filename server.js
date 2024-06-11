b2c32d7a5095a3f3601c013b160ed010476722ee
Luke Warner <cosmo4@byui.edu>
2024-06-10 18:17:48 -0600

    added index.html route to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const path = require('path');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
@@ -9,33 +10,42 @@ const swaggerDocument = require('./swagger.json');
 const app = express();
 
 const startServer = () => {
-    const legoSetRoute = require('./routes/legoSets');
+  const legoSetRoute = require('./routes/legoSets');
 
-    app.use(bodyParser.json())
-    .use(cors())
+  app.use(cors())
+    .use(bodyParser.json())
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
-    .use('/legoSets', legoSetRoute)
+    .use(express.static(path.join(__dirname, 'public')))
     .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-    })
-    .use((err, req, res, next) => {
-      res.status(500).json({ message: err });
-    });
-}
+    .use('/legoSets', legoSetRoute);
+
+  // Serve index.html for the root URL
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+
+  // Error handling middleware
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
+  });
+
+  // Start listening for connections
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+};
 
 const port = 8080;
 
 mongodb.initDb((err) => {
   if (err) {
-    console.log(err);
+    console.error(err);
+    process.exit(1);
   } else {
-    app.listen(port, () => {
-      console.log(`Connected to DB and listening on port ${port}`);
-      startServer();
-    });
+    console.log('Connected to the database');
+    startServer();
   }
 });
 
